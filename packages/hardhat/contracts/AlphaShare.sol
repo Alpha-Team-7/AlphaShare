8411d29c369fcca6dc90db55ec1c3b2c098ba5ec
Godswill Udoh <godswilludoh@gmail.com>
2022-04-15 12:52:44 +0100

    updated natspec comments

--- packages/hardhat/contracts/AlphaShare.sol
+++ packages/hardhat/contracts/AlphaShare.sol
@@ -20,7 +20,7 @@ contract AlphaShare {
     uint256 public fileCounter = 1;
 
     struct File {
-        uint id;
+        uint256 id;
         string key;
         string ipfsHash;
         address owner;
@@ -42,34 +42,54 @@ contract AlphaShare {
 
     modifier hasAccess(uint256 fileId) {
         require(
-            msg.sender == files[fileId].owner || files[fileId].visibility || files[fileId].sharedWith.contains(msg.sender),
+            msg.sender == files[fileId].owner ||
+                files[fileId].visibility ||
+                files[fileId].sharedWith.contains(msg.sender),
             "You dont have access to this file"
         );
         _;
     }
 
     /** EVENTS */
-    event AddFile(uint id, string key, string ipfsHash, uint size, uint createdAt, bool visibility, address owner);
-    event StartFileShare(uint id, address owner, address sharee);
-    event StopFileShare(uint id, address owner, address sharee);
-    event UpdateVisibility(uint id, bool visibility);
+    event AddFile(
+        uint256 id,
+        string key,
+        string ipfsHash,
+        uint256 size,
+        uint256 createdAt,
+        bool visibility,
+        address owner
+    );
 
+    ///@param id The id of each file
+    ///@param owner The owner address of the file shared
+    ///@param sharee The address of user the file is shared with
+    event StartFileShare(uint256 id, address owner, address sharee);
+    event StopFileShare(uint256 id, address owner, address sharee);
+    event UpdateVisibility(uint256 id, bool visibility);
+
+    ///@notice function to add file
+    ///@dev stores the public variables key, ipfsHash and size
+    ///@param key the key of each file relating to the frontend
+    ///@param ipfsHash the unique hash used to reference each file
+    ///@param size the size of individual files
+    ///@param visibility grants or revokes access to files
     function addFiles(
         string[] calldata key,
         string[] calldata ipfsHash,
         uint256[] calldata size,
         bool visibility
     ) public {
-        for(uint i = 0; i < key.length; i++){
+        for (uint256 i = 0; i < key.length; i++) {
             addFile(key[i], ipfsHash[i], size[i], visibility);
         }
     }
 
-    function addFolder(
-        string calldata key
-    ) public {
-        File storage file = files[fileCounter]; 
-        
+    ///@dev incrementing fileCounter to add folders
+    ///@notice function to add folders
+    function addFolder(string calldata key) public {
+        File storage file = files[fileCounter];
+
         file.key = key;
         file.owner = msg.sender;
         file.visibility = true;
@@ -79,14 +99,21 @@ contract AlphaShare {
         fileCounter++;
     }
 
-    function startFileShares(uint256[] calldata fileIds, address[] memory addresses)
-        public
-    {
+    ///@notice function to enable address owner to share files
+    ///@dev stores the fileIds and addresses
+    ///@param fileIds the id used to keep track of each file
+    ///@param addresses user address for file sharing
+    function startFileShares(
+        uint256[] calldata fileIds,
+        address[] memory addresses
+    ) public {
         for (uint256 i = 0; i < fileIds.length; i++) {
             startFileShare(fileIds[i], addresses);
         }
     }
 
+    ///@dev removes fileId and address to prevent file share
+    ///@notice function to stop file sharing, called by file owner
     function stopShare(uint256 fileId, address[] calldata addresses)
         public
         fileOwner(fileId)
@@ -100,6 +127,7 @@ contract AlphaShare {
         }
     }
 
+    ///@dev takes in fileIds and visibility parameters to update file access
     function updateFilesAccess(uint256[] calldata fileIds, bool visibility)
         public
     {
@@ -108,45 +136,81 @@ contract AlphaShare {
         }
     }
 
+    ///@dev uses msg.sender values to retrieve all address owner files
+    ///@return retreiveOwnedFiles The files that belong to the address owner
     function retreiveOwnedFiles()
         public
         view
-        returns (uint[] memory, string[] memory, string[] memory, uint[] memory, uint[] memory, bool[] memory)
+        returns (
+            uint256[] memory,
+            string[] memory,
+            string[] memory,
+            uint256[] memory,
+            uint256[] memory,
+            bool[] memory
+        )
     {
         return retrieveFiles(ownedFiles[msg.sender].values());
     }
 
+    ///@dev uses msg.sender values to retrieve the files address owner was given access to by others
+    ///@return retreiveFilesSharedWithMe The files shared with the address owner
     function retreiveFilesSharedWithMe()
         public
         view
-        returns (uint[] memory, string[] memory, string[] memory, uint[] memory, uint[] memory, bool[] memory)
+        returns (
+            uint256[] memory,
+            string[] memory,
+            string[] memory,
+            uint256[] memory,
+            uint256[] memory,
+            bool[] memory
+        )
     {
         return retrieveFiles(sharedWithMe[msg.sender].values());
     }
 
+    ///@dev uses msg.sender values to retrieve the file address owner gave others access to
+    ///@return retrieveFilesSharedByMe The files shared by the address owner
     function retrieveFilesSharedByMe()
         public
         view
-        returns (uint[] memory, string[] memory, string[] memory, uint[] memory, uint[] memory, bool[] memory)
+        returns (
+            uint256[] memory,
+            string[] memory,
+            string[] memory,
+            uint256[] memory,
+            uint256[] memory,
+            bool[] memory
+        )
     {
         return retrieveFiles(sharedByMe[msg.sender].values());
     }
 
+    ///@dev retrieveFilesSharedByMe, retreiveOwnedFiles, retrieveFilesSharedWithMe functions make use of the retrieveFiles function
     function retrievePublicFiles()
         public
         view
-        returns (uint[] memory, string[] memory, string[] memory, uint[] memory, uint[] memory, bool[] memory)
+        returns (
+            uint256[] memory,
+            string[] memory,
+            string[] memory,
+            uint256[] memory,
+            uint256[] memory,
+            bool[] memory
+        )
     {
         return retrieveFiles(publicFiles.values());
     }
 
-    function retrieveAddressSharedWith (uint fileId) public view
-        fileOwner(fileId) returns (address[] memory)
+    function retrieveAddressSharedWith(uint256 fileId)
+        public
+        view
+        fileOwner(fileId)
+        returns (address[] memory)
     {
         return files[fileId].sharedWith.values();
     }
-
-
 
     /** INTERNAL FUNCTIONS... */
 
@@ -156,8 +220,8 @@ contract AlphaShare {
         uint256 size,
         bool visibility
     ) internal {
-        File storage file = files[fileCounter]; 
-        
+        File storage file = files[fileCounter];
+
         file.id = fileCounter;
         file.key = key;
         file.ipfsHash = ipfsHash;
@@ -174,7 +238,15 @@ contract AlphaShare {
 
         fileCounter++;
 
-        emit AddFile(file.id, key, ipfsHash, size, block.timestamp, visibility, msg.sender);
+        emit AddFile(
+            file.id,
+            key,
+            ipfsHash,
+            size,
+            block.timestamp,
+            visibility,
+            msg.sender
+        );
     }
 
     function startFileShare(uint256 fileId, address[] memory addresses)
@@ -190,11 +262,13 @@ contract AlphaShare {
         }
     }
 
-    function addRemovePublic(uint fileId, bool visibility) internal {
+    ///@notice function to change visibility of files
+    ///@dev changes visibility based on current state
+    function addRemovePublic(uint256 fileId, bool visibility) internal {
         // Add or remove from public
-        if(visibility){
+        if (visibility) {
             publicFiles.add((fileId));
-        }else{
+        } else {
             publicFiles.remove(fileId);
         }
 
@@ -209,20 +283,27 @@ contract AlphaShare {
         addRemovePublic(fileId, visibility);
     }
 
-    function retrieveFiles(uint[] memory fileIds)
+    function retrieveFiles(uint256[] memory fileIds)
         internal
         view
-        returns (uint[] memory, string[] memory, string[] memory, uint[] memory, uint[] memory, bool[] memory)
+        returns (
+            uint256[] memory,
+            string[] memory,
+            string[] memory,
+            uint256[] memory,
+            uint256[] memory,
+            bool[] memory
+        )
     {
-        uint[]  memory id = new uint[](fileIds.length);
-        string[]  memory key = new string[](fileIds.length);
-        string[]  memory ipfsHash = new string[](fileIds.length);
-        uint[]  memory size = new uint[](fileIds.length);
-        uint[]  memory createdAt = new uint[](fileIds.length);
-        bool[]  memory visibility = new bool[](fileIds.length);
+        uint256[] memory id = new uint256[](fileIds.length);
+        string[] memory key = new string[](fileIds.length);
+        string[] memory ipfsHash = new string[](fileIds.length);
+        uint256[] memory size = new uint256[](fileIds.length);
+        uint256[] memory createdAt = new uint256[](fileIds.length);
+        bool[] memory visibility = new bool[](fileIds.length);
 
         for (uint256 i = 0; i < fileIds.length; i++) {
-            uint fileId = fileIds[i];
+            uint256 fileId = fileIds[i];
             File storage file = files[fileId];
             id[i] = file.id;
             key[i] = file.key;
